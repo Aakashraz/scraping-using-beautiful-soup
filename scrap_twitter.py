4bd10998b49195355faaa13dbbef9344f49212eb
Aakashraz <aakashraz@outlook.com>
2024-08-11 15:25:07 +0545

    search data scraping file added

--- scrap_twitter.py
+++ scrap_twitter.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
@@ -18,13 +17,14 @@ print(type(username), username, password)
 
 chrome_options = Options()
 chrome_options.add_argument("--window-size=1920x1080")
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 url = 'https://twitter.com/'
 driver.get(url)
 driver.maximize_window()
 
 wait = WebDriverWait(driver, 20)
 
+# ----------------------------------    SIGN IN ------------------------------------
 try:
     login = wait.until(
         EC.element_to_be_clickable((By.XPATH, '//a[@data-testid = "loginButton"]'))
@@ -36,29 +36,27 @@ except:
 
 try:
     login_box = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "div.css-175oi2r.r-ywje51.r-nllxps.r-jxj0sb.r-1fkl15p.r-16wqof"))
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "div.css-175oi2r.r-ywje51.r-nllxps.r-jxj0sb.r-1fkl15p.r-16wqof"))
     )
-    # user = WebDriverWait(login_box, 10).until(
-    #     EC.presence_of_element_located((By.XPATH, './/input[@name="text"]'))
-    # )
     user_input = login_box.find_element(By.XPATH, './/input[@name="text"]')
     user_input.send_keys(username)
     # time.sleep(2)
     print('Username successful')
     next_button = WebDriverWait(login_box, 10).until(
-        EC.visibility_of_element_located((By.XPATH, './/button[contains(@class, "css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-ywje51 r-184id4b r-13qz1uu r-2yi16 r-1qi8awa r-3pj75a r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l")]'))
+        EC.visibility_of_element_located((By.XPATH,
+                                          './/button[contains(@class, "css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-ywje51 r-184id4b r-13qz1uu r-2yi16 r-1qi8awa r-3pj75a r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l")]'))
     )
     print("Next button found: ", next_button)
     time.sleep(1)
     next_button.click()
     print("Next button clicked...")
 
-    # this input is used to bypass the captcha/authentication.
+    # this input is used to bypass the twitter captcha/authentication.
     input("Press Enter after completing the authentication...")
 
 
 except Exception as e:
     print(e)
-
 
 driver.quit()
---
+++ search_data_twitter.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+driver = webdriver.Chrome(options=chrome_options)
+url = 'https://x.com/search?q=olympics&src=typed_query&f=top'
+driver.get(url)
+
