5ae30de6872fee3c7b82b9903706d8461f0f6cd8
Aakashraz <aakashraz@outlook.com>
2024-08-25 00:40:45 +0545

    scrapy installed

---
+++ scrapy/spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class WorldometersSpider(scrapy.Spider):
+    name = "worldometers"
+    allowed_domains = ["www.worldometers.info"]
+    start_urls = ["https://www.worldometers.info/world-population/nepal-population"]
+
+    def parse(self, response):
+        pass
