63496db07c24b687bc1998c2df781f026de05118
Aakashraz <aakashraz@outlook.com>
2024-08-27 00:44:12 +0545

    used the parse function inside scrapy

--- scrapy/spider_tutorial/spider_tutorial/spiders/worldometers.py
+++ scrapy/spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -3,8 +3,19 @@ import scrapy
 
 class WorldometersSpider(scrapy.Spider):
     name = "worldometers"
-    allowed_domains = ["www.worldometers.info"]
-    start_urls = ["https://www.worldometers.info/world-population/nepal-population"]
+    allowed_domains = ["www.worldometers.info/"]
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country']
 
     def parse(self, response):
-        pass
+        # title = response.xpath('//h1/text()').get()
+        countries = response.xpath('//td/a')
+
+        for country in countries:
+            country_name = country.xpath('.//text()').get()
+            # the following will yield, what's inside the href attribute
+            link = country.xpath('.//@href').get()
+
+            yield {
+                'link': link,
+                'countryName': country_name,
+            }
