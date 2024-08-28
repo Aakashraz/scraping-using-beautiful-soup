961da62af473a5b2bba6279af1899c7ade2d7cc2
Aakashraz <aakashraz@outlook.com>
2024-08-29 01:31:49 +0545

    comment about the CrawlSpider and Rule class

--- scrapy/spider_tutorial/spider_tutorial/spiders/bookscraper.py
+++ scrapy/spider_tutorial/spider_tutorial/spiders/bookscraper.py
@@ -11,6 +11,10 @@ class BookSpider(CrawlSpider):
         Rule(LinkExtractor(allow="catalogue/category")),
         Rule(LinkExtractor(allow="catalogue", deny="category"), callback='parse_item'),
     )
+    # In Scrapy, when using CrawlSpider and Rule, you can specify callback methods using strings.
+    # Scrapy will automatically handle finding and calling these methods on the spider instance,
+    # which is why self isn't needed explicitly in your code snippet. However, when coding outside of such a context,
+    # or if you want explicit control, you would use self.
 
     def parse_item(self, response):
         yield{
