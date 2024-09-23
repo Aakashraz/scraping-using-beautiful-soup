fdf25f257da5300d244ea63574ee35cd1002fafc
Aakashraz <aakashraz@outlook.com>
2024-09-23 16:44:08 +0545

    added employee_log.py learn about handler while logging

--- scrapy/files_to_scrape/files_to_scrape/log.py
+++ scrapy/files_to_scrape/files_to_scrape/log.py
@@ -1,15 +1,28 @@
 import logging
+# import employee_log
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s: %(funcName)s')
+file_handler = logging.FileHandler('test.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+# logging.basicConfig(filename='test.log', level=logging.DEBUG,
+#                     format='%(asctime)s-%(name)s-%(levelname)s-%(message)s-%(funcName)s')
 
 
 def divide(x, y):
     return x / y
 
 
-num1 = 43
-num2 = 54
+num1 = 4375
+num2 = 545
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s -%(funcName)s')
+
 if __name__ == "__main__":
     value = divide(num1, num2)
-    logging.debug(f"Value: {value}")
+    logger.debug(f"Value: {value}")
+
+    print(f'this is inside the log.py: {value}')
