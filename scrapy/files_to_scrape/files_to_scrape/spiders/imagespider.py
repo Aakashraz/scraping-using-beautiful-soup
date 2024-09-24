37e2ed6c9abc0bf07c3ab3a731cdbd6665805860
Aakashraz <aakashraz@outlook.com>
2024-09-24 23:38:10 +0545

    creating FilesToScrapeItem() instance in imagespider.py using ItemLoader()

--- scrapy/files_to_scrape/files_to_scrape/spiders/imagespider.py
+++ scrapy/files_to_scrape/files_to_scrape/spiders/imagespider.py
@@ -8,4 +8,11 @@ class ImageToScrapeSpider(scrapy.Spider):
     start_urls = ['https://books.toscrape.com']
 
     def parse(self, response):
-        pass
+        articles = response.xpath('//article[@class="product_pod"]')
+        for article in articles:
+            loader = Itemloader(item=FilesToScrapeItem(), selector=article)
+            relative_url = article.xpath('.//div[@class="image_container"]/a/img/@src').get()
+            absolute_url = response.urljoin(relative_url)
+            loader.add_value('image_urls', absolute_url)
+            loader.add_xpath('book_name', './/h3/a/@title')
+            yield loader.load_item()
